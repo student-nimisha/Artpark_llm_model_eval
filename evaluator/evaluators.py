b4fea2be39dde66289d88cef977f48e49e2b0198
Nimisha Iyer <155137074+student-nimisha@users.noreply.github.com>
2026-08-01 17:03:14 +0530

    Create evaluators.py

---
+++ evaluator/evaluators.py
@@ -0,0 +1,27 @@
+from datasets.hf_dataset_loader import load_hf_dataset
+
+
+def run_evaluation(config):
+
+    print("=" * 60)
+    print("Loading Dataset")
+    print("=" * 60)
+
+    dataset = load_hf_dataset(config)
+
+    print("\nDataset Loaded Successfully\n")
+
+    print("Displaying first 5 samples:\n")
+
+    for i, sample in enumerate(dataset):
+
+        print(f"Sample {i+1}")
+
+        print("Text :", sample["text"])
+
+        print("Label:", sample["label"])
+
+        print("-" * 60)
+
+        if i == 4:
+            break
